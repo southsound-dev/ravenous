58c52ae284a5de61bf1b0df5ec871327217aa36a
Juan Carlos Vargas <Inicio@MacBook-Pro-de-Juan.local>
2020-05-26 00:59:11 +0200

    ravenous project day 2

--- src/components/Business/Business.js
+++ src/components/Business/Business.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import './Business.css';
+import '../Business/Business.css'
 
 const business = {
-    imageSrc: 'https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg',
+  imageSrc: 'https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg',
   name: 'MarginOtto Pizzeria',
   address: '1010 Paddington Way',
   city: 'Flavortown',
@@ -13,28 +13,30 @@ const business = {
   reviewCount: 90
 };
 
-className Business extends React.Component {
-    render() {
-        return (
-        <div className="Business">
-  <div className="image-container">
-    <img src='https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg' alt=''/>
-  </div>
-  <h2>{business.name}</h2>
-  <div className="Business-information">
-    <div className="Business-address">
-      <p>{business.address}</p>
-      <p>{business.city}</p>
-      <p>{business.zipcode}</p>
-    </div>
-    <div className="Business-reviews">
-      <h3>{business.category}</h3>
-      <h3 className="rating">{business.rating} stars</h3>
-      <p>{business.reviewCount}</p>
-    </div>
-  </div>
-</div>
-        )
-    }
+class Business extends React.Component {
+  render() {
+    return (
+      <div className="Business">
+        <div className="image-container">
+          <img src='https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg' alt='' />
+        </div>
+        <h2>{business.name}</h2>
+        <div className="Business-information">
+          <div className="Business-address">
+            <p>{business.address}</p>
+            <p>{business.city}</p>
+            <p>{business.zipcode}</p>
+          </div>
+          <div className="Business-reviews">
+            <h3>{business.category}</h3>
+            <h3 className="rating">{business.rating} stars</h3>
+            <p>{business.reviewCount}</p>
+          </div>
+        </div>
+      </div>
+    )
+  }
 }
+
+export default Business;
 
