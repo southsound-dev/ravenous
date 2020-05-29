f96107e6e00ba7ab01be60f2ea0912f143e39243
Juan Carlos Vargas <Inicio@MacBook-Pro-de-Juan.local>
2020-05-30 00:36:34 +0200

    ravenous project part 3 day 1

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,38 +1,56 @@
 import React from 'react';
 import '../SearchBar/SearchBar.css';
 
-const sortByOptions = {
-    'Best Match': 'best_match',
-    'Highest Rated': 'rating',
-    'Most Reviewed': 'review_count'
-}
+
 
 class Search extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      term: '',
+      location: '',
+      sortBy: 'best_match'
+    };
+    this.sortByOptions = {
+      'Best Match': 'best_match',
+      'Highest Rated': 'rating',
+      'Most Reviewed': 'review_count'
+    };
+  }
+
+  getSortByClass(sortByOption) {
+    if (sortByOption === this.state.sortBy) {
+      return 'active';
+    }
+    else {
+      return '';
+    }}
+
     renderSortByOptions() {
-      return Object.keys(sortByOptions).map(sortByOption => {
-        let sortByOptionValue = sortByOptions[sortByOption];
+      return Object.keys(this.sortByOptions).map(sortByOption => {
+        let sortByOptionValue = this.sortByOptions[sortByOption];
         return <li key={sortByOptionValue}>{sortByOption}</li>;
       });
     }
 
     render() {
-        return (
-            <div className="SearchBar">
-<div className="SearchBar-sort-options">
-<ul>
-  {this.renderSortByOptions()}
-</ul>
-</div>
-<div className="SearchBar-fields">
-<input placeholder="Search Businesses" />
-<input placeholder="Where?" />
-</div>
-<div className="SearchBar-submit">
-<a href="www.#.com">Let's Go</a>
-</div>
-</div>
-        )
+      return (
+        <div className="SearchBar">
+          <div className="SearchBar-sort-options">
+            <ul>
+              {this.renderSortByOptions()}
+            </ul>
+          </div>
+          <div className="SearchBar-fields">
+            <input placeholder="Search Businesses" />
+            <input placeholder="Where?" />
+          </div>
+          <div className="SearchBar-submit">
+            <a href="www.#.com">Let's Go</a>
+          </div>
+        </div>
+      )
     }
 
-}
-export default Search;
+  }
+  export default Search;
